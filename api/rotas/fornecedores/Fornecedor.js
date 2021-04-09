09553e20c509c0198ad16003a0afdf239e7f7e72
plinio <plinio.garcia@ifc.edu.br>
2021-04-09 09:46:50 -0300

    persistindo dados (post)

---
+++ api/rotas/fornecedores/Fornecedor.js
@@ -0,0 +1,30 @@
+const TabelaFornecedor = require('./TabelaFornecedor')
+class Fornecedor{
+
+    constructor({id, empresa, email, categoria, dataCriacao, dataAtualizacao, versao}){
+        this.id = id;
+        this.empresa = empresa,
+        this.email = email,
+        this.categoria = categoria,
+        this.dataCriacao = dataCriacao,
+        this.dataAtualizacao = dataAtualizacao,
+        this.versao = versao
+    }
+
+    async criar(){
+
+        const resultado = await TabelaFornecedor.inserir({
+            empresa: this.empresa,
+            email: this.email,
+            categoria: this.categoria
+        })
+
+        //após inserir ja terei os demais dados que vieram do banco e completar:
+        this.id = resultado.id;
+        this.dataCriacao = resultado.dataCriacao,
+        this.dataAtualizacao = resultado.dataAtualizacao,
+        this.versao = resultado.versao
+    }
+}
+
+module.exports = Fornecedor
--- api/rotas/fornecedores/TabelaFornecedor.js
+++ api/rotas/fornecedores/TabelaFornecedor.js
@@ -3,5 +3,8 @@ const Modelo = require('./modeloTabelaFornecedor')
 module.exports = {
     listar(){
         return Modelo.findAll()
+    },
+    inserir(fornecedor){
+        return Modelo.create(fornecedor)
     }
 }
