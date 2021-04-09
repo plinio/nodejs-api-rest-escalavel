ed9424df9cf76f1a45dfea3979ca9dba81e518b4
plinio <plinio.garcia@ifc.edu.br>
2021-04-09 09:35:04 -0300

    listando fornecedores (get)

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -1,7 +1,12 @@
 const roteador = require('express').Router()
+const TabelaFornecedor = require('./TabelaFornecedor')
 
-roteador.use('/', (requisicao, resposta) => {
-    resposta.send('OK')
+roteador.use('/', async (requisicao, resposta) => {
+    //vai esperar pegar os dados para enviar a resposta
+    const resultados = await TabelaFornecedor.listar()
+    resposta.send(
+        JSON.stringify(resultados)
+    )
 })
 
 module.exports = roteador;
