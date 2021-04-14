1c7cc6365e6b87a05c88c8f7d8e445a81ca4d850
plinio <plinio.garcia@ifc.edu.br>
2021-04-14 08:40:26 -0300

    npm install jsontoxml - alterando o serializador para aceitar XML

--- api/Serializador.js
+++ api/Serializador.js
@@ -1,15 +1,34 @@
 const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
+const jsontoxml = require('jsontoxml')
 
 class Serializador {
     json(dados){
         return JSON.stringify(dados)
     }
 
+    xml(dados){
+        let tag = this.tagSingular
+
+        if(Array.isArray(dados)){
+            tag = this.tagPlural
+
+            dados = dados.map((item =>{
+                return {
+                    [this.tagSingular]: item
+                }
+            }))
+        }
+        return jsontoxml({[tag]: dados})
+    }
+
     serializar(dados){
+        dados = this.filtrar(dados);
+
         if(this.contentType === 'application/json'){
-            return this.json(
-                this.filtrar(dados)
-            )
+            return this.json(dados)
+        }
+        if(this.contentType === 'application/xml'){
+            return this.xml(dados)
         }
 
         throw new ValorNaoSuportado(this.contentType)
@@ -44,11 +63,13 @@ class SerializadorFornecedor extends Serializador{
         super();
         this.contentType = contentType;
         this.camposPublicos = ['id', 'empresa', 'categoria'].concat(camposExtras || []);
+        this.tagSingular = 'fornecedor';//usa no XML
+        this.tagPlural = 'fornecedores'//usa no XML
     }
 }
 
 module.exports = {
     Serializador: Serializador,
     SerializadorFornecedor: SerializadorFornecedor,
-    formatosAceitos: ['application/json']
+    formatosAceitos: ['application/json', 'application/xml']
 }
