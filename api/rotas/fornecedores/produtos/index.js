0b1f27f0038c4774786465e9a61882e7e18a157c
plinio <plinio.garcia@ifc.edu.br>
2021-04-14 09:27:29 -0300

    rota para reclamacoes de um produto

--- api/rotas/fornecedores/produtos/index.js
+++ api/rotas/fornecedores/produtos/index.js
@@ -1,5 +1,6 @@
 const roteador = require('express').Router({mergeParams: true})
 const Tabela = require('./TabelaProduto')
+const roteadorReclamacoes = require('./reclamacoes')
 
 roteador.get('/', async (requisicao, resposta)=>{
     const produtos = await Tabela.listar(requisicao.params.idFornecedor)
@@ -7,5 +8,7 @@ roteador.get('/', async (requisicao, resposta)=>{
         JSON.stringify(produtos)
     )
 })
+
+roteador.use('/:idProduto/reclamacoes', roteadorReclamacoes)
 
 module.exports = roteador
