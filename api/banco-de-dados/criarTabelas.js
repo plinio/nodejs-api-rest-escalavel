5105a6bf77cf621c6dfa887e8083212db2520692
plinio <plinio.garcia@ifc.edu.br>
2021-04-14 09:08:57 -0300

    alterando script de criacao de tabelas e executando com node api/banco-de-dados/criarTabelas.js

--- api/banco-de-dados/criarTabelas.js
+++ api/banco-de-dados/criarTabelas.js
@@ -1,7 +1,13 @@
-const ModeloTabela = require('../rotas/fornecedores/modeloTabelaFornecedor')
+const modelos = [
+    require('../rotas/fornecedores/modeloTabelaFornecedor'),
+    require('../rotas/fornecedores/produtos/ModeloTabelaProduto')
+]
 
-ModeloTabela
-    .sync()
-    .then(()=> console.log('Tabela Criada com sucesso'))
-    .catch(console.log)
-    
+async function criarTabelas(){
+    for(let contador =0; contador < modelos.length; contador++){
+        const modelo = modelos[contador];
+        await modelo.sync()
+    }
+}
+
+criarTabelas();
